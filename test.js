9d1811ef05689608779a1f90b8add4c0425fe78c
Jay Williams <course@jaywilliams.me>
2017-11-16 17:56:03 +0000

    + Tests

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ var library = require("./library");
 var inp = "I am the captain";
 console.log("Testing with input: " + inp);
 console.log("Returned: " + library.encode(inp))
+
+var inp = "Void is da best developer ukno";
+console.log("Testing with input: " + inp);
+console.log("Returned: " + library.encode(inp))
